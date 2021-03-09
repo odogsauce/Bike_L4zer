623758bb63769596c0e0e8c4f0f043d1745d0d1b
thibault rahou <trahou33@gmail.com>
2021-03-09 16:37:35 +0100

    map install

--- app/javascript/components/buttontop.js
+++ app/javascript/components/buttontop.js
@@ -1,24 +1,24 @@
-//Get the button:
-myBtn = document.getElementById("myBtn");
+// //Get the button:
+// myBtn = document.getElementById("myBtn");
 
-// When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {scrollFunction()};
+// // When the user scrolls down 20px from the top of the document, show the button
+// window.onscroll = function() {scrollFunction()};
 
-function scrollFunction() {
-  if (document.body.scrollTop > 5 || document.documentElement.scrollTop > 5) {
-    mybutton.style.display = "block";
-  } else {
-    mybutton.style.display = "none";
-  }
-}
+// function scrollFunction() {
+//   if (document.body.scrollTop > 5 || document.documentElement.scrollTop > 5) {
+//     mybutton.style.display = "block";
+//   } else {
+//     mybutton.style.display = "none";
+//   }
+// }
 
-// When the user clicks on the button, scroll to the top of the document
-function topFunction() {
-  document.body.scrollTop = 0; // For Safari
-  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-}
+// // When the user clicks on the button, scroll to the top of the document
+// function topFunction() {
+//   document.body.scrollTop = 0; // For Safari
+//   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+// }
 
 
 
-export { scrollFunction };
-export { topFunction };
+// export { scrollFunction };
+// export { topFunction };
